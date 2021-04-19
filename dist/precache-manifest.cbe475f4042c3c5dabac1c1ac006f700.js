3c8c75fef1de1de2085f12dbc95f4a2ee32cc2ea
dev-ale <ale.iphone@gmail.com>
2021-04-19 22:44:12 +0200

    init

---
+++ dist/precache-manifest.cbe475f4042c3c5dabac1c1ac006f700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50be850cdb85a59c226b",
+    "url": "/css/chunk-vendors.6603192c.css"
+  },
+  {
+    "revision": "48b426a51c1b00363192549223ac13a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb09e41242eee1357e85",
+    "url": "/js/app.6c11f3cc.js"
+  },
+  {
+    "revision": "50be850cdb85a59c226b",
+    "url": "/js/chunk-vendors.5113a707.js"
+  },
+  {
+    "revision": "79853f960c4eb754a1e361ed02123ac2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
