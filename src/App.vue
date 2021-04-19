47af93bbb4e58971012cf225aadb3543c7f9b976
dev-ale <ale.iphone@gmail.com>
2021-04-19 21:46:25 +0200

    added vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,86 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+        app
+        color="primary"
+        dark
     >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
       <v-spacer></v-spacer>
-      <h2>fhnw pinboard - webeC</h2>
+
+      <div class="d-flex align-center">
+        <h2>Corona-Navigator</h2>
+      </div>
+
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/dev-ale/WebEngineering"
-        target="_blank"
-        text
+          href="https://gitlab.fhnw.ch/wodss-wowm-group-bs-bl/webservice-bl-bs"
+          target="_blank"
+          text
       >
-        <v-icon>mdi-github</v-icon>
+        <span class="mr-2">GitLab</span>
+        <v-icon>mdi-gitlab</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <home/>
+      <v-container>
+        <v-row justify="center">
+          <v-col align=" center" cols="6">
+            <v-card
+                max-width="475"
+                class="mx-auto"
+            >
+              <v-toolbar
+                  color="teal"
+                  dark
+              >
+                <v-spacer></v-spacer>
+                <v-toolbar-title>sampledata</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-list dense v-for="incidence of incidences">
+                <v-list-item>
+                  <v-list-item-title>
+                    <h2>{{ incidence.name }}</h2>
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    <v-chip color="teal" dark>{{ incidence.incident }}</v-chip>
+                  </v-list-item-subtitle>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
-
-import Home from "@/views/Home";
 export default {
   name: 'App',
-
   components: {
-    Home
-
   },
-
   data: () => ({
-    //
+    incidences: [{
+      timeStamp: "timestamp",
+      name: "Basel",
+      canton: "BS",
+      date: "2021-04-19",
+      incident: "272",
+    },
+      {
+        timeStamp: "timestamp",
+        name: "Aesch",
+        canton: "BL",
+        date: "2021-04-19",
+        incident: "201",
+      }]
   }),
 };
 </script>
