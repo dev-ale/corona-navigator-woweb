114aaa10508dbf84afe78c93aafaebc0a7c7af7f
dev-ale <ale.iphone@gmail.com>
2021-04-23 13:52:25 +0200

    added map

--- src/components/Auto.vue
+++ src/components/Auto.vue
@@ -1,13 +1,55 @@
 <template>
   <v-container>
     <h2>Auto</h2>
+    <GmapMap
+        :center="entryPoint"
+        :zoom="10"
+        style="width: 1150px; height: 600px"
+    >
+      <GmapMarker
+          v-for="(m, index) in markers"
+          :key="index"
+          :position="m.position"
+      />
+    </GmapMap>
   </v-container>
 
 </template>
 
 <script>
 export default {
-  name: "Auto"
+  name: "Auto",
+  data: () => ({
+    markers: [
+        {
+          name: 'start',
+          position: {
+            lat:47.559601,
+            lng:7.588576
+          }
+        },
+        {
+          name: 'end',
+          position: {
+            lat:47.4722,
+            lng:7.6194
+          }
+        },
+      {
+        name: 'pause',
+        position: {
+          lat:47.5167,
+          lng:7.6167
+        }
+      }
+    ],
+
+    entryPoint: {
+      lat:47.4722,
+      lng:7.6194
+    }
+  }),
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import './plugins/axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from 'vue2-google-maps'
+const dotenv = require('dotenv');
+dotenv.config();
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLEMAPS_API_KEY,
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+});
 
 Vue.config.productionTip = false
 
