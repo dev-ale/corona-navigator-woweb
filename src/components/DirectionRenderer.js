36dc2b99b039fdaca9f7b87dc1c7c75ce87a9c0a
pascal <pascal.buerge@students.fhnw.ch>
2021-04-23 17:05:28 +0200

    pause slot frontend

--- src/components/DirectionRenderer.js
+++ src/components/DirectionRenderer.js
@@ -14,24 +14,41 @@ export default MapElementFactory({
     props: {
         origin: { type: Object },
         destination: { type: Object },
+        location: {type: String},
         travelMode: { type: String }
     },
 
     afterCreate(directionsRenderer) {
         let directionsService = new window.google.maps.DirectionsService();
-
         this.$watch(
-            () => [this.origin, this.destination, this.travelMode],
+            () => [this.origin, this.destination, this.travelMode, this.location],
             () => {
-                let { origin, destination, travelMode } = this;
+                let { origin, destination, travelMode, location } = this;
                 if (!origin || !destination || !travelMode) return;
 
-                directionsService.route(
-                    {
+                let request;
+                if(location){
+                    request = {
+                        origin,
+                        destination,
+                        waypoints: [
+                            {
+                                location: location,
+                                stopover: true,
+                            }
+                        ],
+                        travelMode
+                    }
+                }else {
+                    request = {
                         origin,
                         destination,
                         travelMode
-                    },
+                    }
+                }
+
+                directionsService.route(
+                    request,
                     (response, status) => {
                         if (status !== "OK") return;
                         directionsRenderer.setDirections(response);
