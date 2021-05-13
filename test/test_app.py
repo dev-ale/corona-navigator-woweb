bfc3acf7bb585a6f77d481c633854fee380bce87
Cedric Purtschert <cedric.purtschert@students.fhnw.ch>
2021-05-13 10:20:32 +0200

    added new tests for api

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,34 @@
 from pytest_helper import client
+import json
+from jsonschema import validate
 
-def test_api_incidences(client):
+schema_incidence = {
+    "type":"array",
+    "items": {
+        "type" : "object",
+        "properties" : {
+            "canton" : {"type" : "string"},
+            "date" : {"type" : "string"},
+            "incident" : {"type" : "number"},
+            "name" : {"type" : "string"}
+        }
+    }
+}
+
+
+def test_if_api_is_reachable(client):
     response = client.get("/api/incidences")
-    assert response.json == []
+    assert response.status_code == 200
+
+
+def test_get_incidences_from_db(client):
+    response = client.get("/api/incidences")
+    body = response.json
+    assert len(body) == 2
+
+def test_get_incidences_and_check_schema(client):
+    response = client.get("/api/incidences")
+    body = response.json
+    validate(instance=body, schema=schema_incidence)
+    assert response.headers['Content-Type'] == "application/json"
+    assert len(body) == 2
