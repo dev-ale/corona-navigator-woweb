cb9ea573f61aa7fca9a25fad6ea34ff74c6afc3f
Cedric Purtschert <cedric.purtschert@students.fhnw.ch>
2021-05-13 10:32:47 +0200

    Added delete_many statement to prevent deprecated warning

--- test/pytest_helper.py
+++ test/pytest_helper.py
@@ -1,19 +1,23 @@
 import sys
 import os
 import pytest
-ROOT_DIR = os.path.dirname(os.path.abspath('app.py'))   # TODO, find better solution
+
+ROOT_DIR = os.path.dirname(os.path.abspath('app.py'))  # TODO, find better solution
 sys.path.append(ROOT_DIR)
 import app
 
 from flask_pymongo import PyMongo
 
+
 @pytest.fixture
 def client():
     app.app.config['TESTING'] = True
     app.mongo = init_db()
+    reset_db(app.mongo)
     seed_db(app.mongo)
     yield app.app.test_client()
     reset_db(app.mongo)
+
 
 @pytest.fixture
 def database():
@@ -24,12 +28,14 @@ def init_db():
     app.app.config['MONGO_URI'] = 'mongodb://localhost:27017/testDB'
     return PyMongo(app.app)
 
+
 def seed_db(db):
     db.db.incidences.insert_many([
         {'canton': 'ZH', 'date': '2021-05-12', 'incident': 100.90817356205852, 'name': 'Aeugst am Albis'},
-        {'canton': 'BL', 'date': '2021-05-12', 'incident': 241.0,              'name':'Oberwil'}
+        {'canton': 'BL', 'date': '2021-05-12', 'incident': 241.0, 'name': 'Oberwil'}
     ])
 
 
 def reset_db(db):
-    db.db.incidences.remove()
+    query = {"incident": {"$gt": -1}}
+    db.db.incidences.delete_many(query)
