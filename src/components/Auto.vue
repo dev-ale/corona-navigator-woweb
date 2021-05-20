feb8acfee25bc9fd700f4d8132925cf41ab58380
pascal <pascal.buerge@students.fhnw.ch>
2021-05-20 11:17:02 +0200

    in view change var

--- src/components/Auto.vue
+++ src/components/Auto.vue
@@ -51,8 +51,8 @@
                 <h2>{{ start }}</h2>
               </v-list-item-title>
               <v-list-item-subtitle>
-                <v-chip v-if="this.endCity.incident != null" color="primary" dark>{{ this.endCity.incident }}</v-chip>
-                <v-chip v-if="this.endCity.incident === null" outlined color="primary">Loading!</v-chip>
+                <v-chip v-if="this.startCity.incident != null" color="primary" dark>{{ this.startCity.incident }}</v-chip>
+                <v-chip v-if="this.startCity.incident === null" outlined color="primary">Loading!</v-chip>
               </v-list-item-subtitle>
             </v-list-item>
 
@@ -71,7 +71,7 @@
                 <h2>{{ end }}</h2>
               </v-list-item-title>
               <v-list-item-subtitle>
-                <v-chip v-if="this.startCity.incident != null" color="primary" dark>{{ this.startCity.incident}}</v-chip>
+                <v-chip v-if="this.endCity.incident != null" color="primary" dark>{{ this.endCity.incident}}</v-chip>
                 <v-chip v-if="this.startCity.incident === null" outlined color="primary">Loading!</v-chip>
               </v-list-item-subtitle>
             </v-list-item>
